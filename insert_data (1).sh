ca84dbf4881a41dc43a58482b35d782d9ec0909d
Rosemarie Crisostomo Beltran <149545257+scoutfinch18@users.noreply.github.com>
2026-02-21 18:45:59 +0800

    Add files via upload

---
+++ insert_data (1).sh
@@ -0,0 +1,64 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+echo $($PSQL "TRUNCATE TABLE teams, games")
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  # process only if not header
+  if [[ $WINNER != winner ]]
+  then 
+    # get team_id
+    TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+
+    # if not found
+    if [[ -z $TEAM_ID ]]
+    then
+      # insert team name
+      INSERT_NAME_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      if [[ $INSERT_NAME_RESULT == 'INSERT 0 1' ]]
+      then
+        echo Inserted into teams, $WINNER
+      fi
+    fi
+  fi
+
+  # process only if not header
+  if [[ $OPPONENT != opponent ]]
+  then 
+    # get team_id
+    TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+
+    # if not found
+    if [[ -z $TEAM_ID ]]
+    then
+      # insert team name
+      INSERT_NAME_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      if [[ $INSERT_NAME_RESULT == 'INSERT 0 1' ]]
+      then
+        echo Inserted into teams, $OPPONENT
+      fi
+    fi
+  fi
+
+  if [[ $YEAR != year ]]
+  then 
+    # insert game data
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year,round,winner_id,opponent_id,winner_goals,opponent_goals) VALUES('$YEAR','$ROUND','$WINNER_ID','$OPPONENT_ID','$WINNER_GOALS','$OPPONENT_GOALS')")
+    if [[ $INSERT_GAME_RESULT == 'INSERT 0 1' ]]
+    then
+      echo Inserted new game data
+    fi
+  fi
+done
+
+
+
